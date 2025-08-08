ba5ab8e25aa5494df4d6739401faa5ee0671e11e
Josh Estelle <josh.estelle@gmail.com>
2025-08-07 21:41:01 -0400

    Add basic structure for a mobile app. It'll run on iphone, ipad, and desktop

---
+++ EmojiTapper/EmojiTapperMobile/EmojiTapperMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiTapperMobileApp.swift
+//  EmojiTapperMobile
+//
+//  Created by Josh Estelle on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct EmojiTapperMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmojiTapper/EmojiTapperMobileTests/EmojiTapperMobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiTapperMobileTests.swift
+//  EmojiTapperMobileTests
+//
+//  Created by Josh Estelle on 8/7/25.
+//
+
+import Testing
+@testable import EmojiTapperMobile
+
+struct EmojiTapperMobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
