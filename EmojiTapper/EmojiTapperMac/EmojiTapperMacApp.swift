7f5426d2a4b4cae3131884eb64c4852187058c2d
Josh Estelle <josh.estelle@gmail.com>
2025-08-08 11:17:10 -0400

    Added mac build

---
+++ EmojiTapper/EmojiTapperMac/EmojiTapperMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiTapperMacApp.swift
+//  EmojiTapperMac
+//
+//  Created by Josh Estelle on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct EmojiTapperMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmojiTapper/EmojiTapperMacTests/EmojiTapperMacTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiTapperMacTests.swift
+//  EmojiTapperMacTests
+//
+//  Created by Josh Estelle on 8/8/25.
+//
+
+import Testing
+@testable import EmojiTapperMac
+
+struct EmojiTapperMacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
